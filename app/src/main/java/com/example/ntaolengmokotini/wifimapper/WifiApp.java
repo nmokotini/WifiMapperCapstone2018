8baa169e79a80581bedc6610a9a9c4aeea9e0bda
nmokotini <30896445+nmokotini@users.noreply.github.com>
2018-08-16 05:22:30 +0200

    resolved package import errors

--- app/src/main/java/com/example/ntaolengmokotini/wifimapper/WifiApp.java
+++ app/src/main/java/com/example/ntaolengmokotini/wifimapper/WifiApp.java
@@ -1,5 +1,6 @@
 package com.example.ntaolengmokotini.wifimapper;
 
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,14 +10,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.android.net.wifi.WifiInfo;
-import com.android.net.wifi.WifiManager;
+import com.google.android.gms.location.LocationServices;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
 
 
 
 public class WifiApp extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+   // private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,10 @@ public class WifiApp extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+       // mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
     }
 
 
@@ -33,7 +42,6 @@ public class WifiApp extends FragmentActivity implements OnMapReadyCallback {
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -42,13 +50,18 @@ public class WifiApp extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        WifiManager wifiManager = (WifiManager)this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
+        //set number of RSSI Levels
+        final int rssiLevels = 5;
+        //calculate signal level based on  RSSI levels
+        int wifiStrength = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), rssiLevels);
 
-
-
-        // Add a marker in Sydney and move the camera
         LatLng compsci = new LatLng(-33.95686760901559, 18.46106835175306);
         mMap.addMarker(new MarkerOptions().position(compsci).title("Computer Science Building"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(compsci));
     }
+
+
 }
